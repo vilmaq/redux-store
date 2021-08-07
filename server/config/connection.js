89e43e625709ffc4768cc81a4a45c87b14bc64c3
Vilma Qerama <vilma.qerama@hotmail.com>
2021-08-07 21:35:13 +0100

    change db connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,27 +1,27 @@
 const mongoose = require("mongoose");
 
 const DB_NAME = process.env.DB_NAME || "mernshopping";
-const DB_URL = process.env.MONGODB_URI || `mongodb://localhost/${DB_NAME}`;
+// const DB_URL = process.env.MONGODB_URI || `mongodb://localhost/${DB_NAME}`;
 
-const MONGOOSE_OPTIONS = {
-  useUnifiedTopology: true,
-  useCreatedIndex: true,
-  useNewUrlParser: true,
-  useFindAndModify: false,
-};
-module.exports = {
-  DB_URL,
-  MONGOOSE_OPTIONS,
-};
+// const MONGOOSE_OPTIONS = {
+//   useUnifiedTopology: true,
+//   useCreatedIndex: true,
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+// };
+// module.exports = {
+//   DB_URL,
+//   MONGOOSE_OPTIONS,
+// };
 
-// mongoose.connect(
-//   process.env.MONGODB_URI || "mongodb://localhost/mernshopping",
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//   }
-// );
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/mernshopping",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
-// module.exports = mongoose.connection;
+module.exports = mongoose.connection;
