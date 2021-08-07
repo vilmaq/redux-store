2f5527e77d640327ff908ac1f548ee780006fa3a
Vilma Qerama <vilma.qerama@hotmail.com>
2021-08-07 21:19:56 +0100

    add concurrently dependency

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
-  useNewUrlParser: true,
+const DB_NAME = process.env.DB_NAME || "mernshopping";
+const DB_URL = process.env.MONGODB_URI || `mongodb://localhost/${DB_NAME}`;
+
+const MONGOOSE_OPTIONS = {
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+  useCreatedIndex: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+};
+module.exports = {
+  DB_URL,
+  MONGOOSE_OPTIONS,
+};
 
-module.exports = mongoose.connection;
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/mernshopping",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   }
+// );
+
+// module.exports = mongoose.connection;
