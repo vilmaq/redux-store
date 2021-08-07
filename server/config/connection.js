f7a5723633f4d2f2aaebe01ed4b72b6f351a95e9
Vilma Qerama <vilma.qerama@hotmail.com>
2021-08-07 22:08:47 +0100

    add predeploy script

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,19 +1,5 @@
 const mongoose = require("mongoose");
 
-const DB_NAME = process.env.DB_NAME || "mernshopping";
-// const DB_URL = process.env.MONGODB_URI || `mongodb://localhost/${DB_NAME}`;
-
-// const MONGOOSE_OPTIONS = {
-//   useUnifiedTopology: true,
-//   useCreatedIndex: true,
-//   useNewUrlParser: true,
-//   useFindAndModify: false,
-// };
-// module.exports = {
-//   DB_URL,
-//   MONGOOSE_OPTIONS,
-// };
-
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/mernshopping",
   {
